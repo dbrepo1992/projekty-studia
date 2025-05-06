53f744635694f4fc97a5ddcf08e5acf647bcaf93
Damian Bugaj <damianbugajit@gmail.com>
2025-05-06 15:28:24 +0200

    Added private's fields and getters for them

--- java/lab2/Samochod/src/samochod/Samochod.java
+++ java/lab2/Samochod/src/samochod/Samochod.java
@@ -2,11 +2,47 @@ package samochod;
 
 public class Samochod {
 
-    /**
-     * @param args the command line arguments
-     */
+    private String marka;
+    private String model;
+    private int iloscDrzwi;
+    private int pojemnoscSilnika;
+    private double srednieSpalanie;
+
+
+    // Created get functions for each private
+    //              ||
+    //              ||
+    //             \  /
+    //              \/
+
+
+    
+    public String marka() {
+        return marka;
+    }
+
+
+    public String model() {
+        return model;
+    }
+
+
+    public int iloscDrzwi() {
+        return iloscDrzwi;
+    }
+
+
+    public int pojemnoscSilnika() {
+        return pojemnoscSilnika;
+    }
+
+
+    public double srednieSpalanie() {
+        return srednieSpalanie;
+    }
+
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+
 }
