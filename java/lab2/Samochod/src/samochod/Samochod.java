dd1cb7387b0565c7199a76e4cf7809e9cef4b921
Damian Bugaj <damianbugajit@gmail.com>
2025-05-06 18:09:59 +0200

    add more code

--- java/lab2/Samochod/src/samochod/Samochod.java
+++ java/lab2/Samochod/src/samochod/Samochod.java
@@ -2,20 +2,42 @@ package samochod;
 
 public class Samochod {
 
-    private String marka;
+    // (Pola obiektu klasy to te zmienne, które są zadeklarowane na początku klasy i należą do każdego utworzonego obiektu.)
+    //              ||
+    //              ||
+    //             \  /
+    //              \/
+
+    private String marka; // < -- (To jest pole obiektu. Każdy samochód (Samochod) ma swoją własną marka.)
     private String model;
     private int iloscDrzwi;
     private int pojemnoscSilnika;
     private double srednieSpalanie;
     private static int iloscSamochodow = 0;
 
+
+    // Default constructor
     public Samochod() {
-        marka = "nieznana";
-        model = "nieznany";
-        iloscDrzwi = 0;
-        pojemnoscSilnika = 0;
-        srednieSpalanie = 0.0;
+        this.marka = "nieznana"; // (przypisanie domyślnej wartości do pola instancji.)
+        this.model = "nieznany";
+        this.iloscDrzwi = 0;
+        this.pojemnoscSilnika = 0;
+        this.srednieSpalanie = 0.0;
+        iloscSamochodow++; // we increase number of cars (dostęp do statycznego pola klasy, zwiększającego licznik obiektów.)
     }
+
+    // Constructor with all data as params (Parametry konstruktora to zmienne lokalne, które są przekazywane do konstruktora)
+    // String marka_  // to jest parametr. On istnieje tylko w momencie tworzenia obiektu i znika, gdy konstruktor się zakończy.
+    public Samochod(String marka_, String model_, int iloscDrzwi_, int pojemnoscSilnika_, double srednieSpalanie_) {
+        this.marka = marka_;    // (Słowo kluczowe [this] odnosi się do pola obiektu – odróżnia je od nazwy parametru.)
+        this.model = model_;    // (this.model → odnosi się do pola obiektu (czyli model samochodu, który właśnie tworzysz).model_ → to nazwa parametru, który użytkownik podał jako argument w new Samochod(...))
+        this.iloscDrzwi = iloscDrzwi_;
+        this.pojemnoscSilnika = pojemnoscSilnika_;  // Podkreślnik (_) jest częścią nazwy parametru. To tylko konwencja, która pomaga odróżnić parametr od pola.
+        this.srednieSpalanie = srednieSpalanie_;    // oznacza: „ustaw pole marka (należące do obiektu) na wartość parametru marka_.
+
+        iloscSamochodow++;
+    }
+
 
 
     // Created get and set functions for each private
@@ -74,8 +96,6 @@ public class Samochod {
 
 
 
-
-
     // getter srednieSpalanie
     public double getSrednieSpalanie() {
         return srednieSpalanie;
@@ -88,6 +108,17 @@ public class Samochod {
 
     public static void main(String[] args) {
 
+
+        // Miejsce w pamięci jest rezerwowane w momencie wywołania konstruktora przez słowo kluczowe new.
+        // Tworzony jest obiekt klasy Samochod w pamięci (na sterta),
+        // Java rezerwuje miejsce na jego pola (marka, model, iloscDrzwi, itd.),
+        // Następnie wywołuje odpowiedni konstruktor, który te pola inicjalizuje (czyli wypełnia je wartościami).
+        // Można to porównać do:
+        // new = budujemy nowy samochód (baza)
+        // konstruktor = konfigurujemy ten samochód (marka, model, silnik, itd.)
+
+        Samochod s1 = new Samochod();
+        Samochod s2 = new Samochod("Toyota", "Corolla", 4, 1600, 6.5);
     }
 
 }
