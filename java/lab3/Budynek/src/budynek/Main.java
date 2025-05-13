ad3506964e487861467969b9576aaed2c07d6a16
Damian Bugaj <damianbugajit@gmail.com>
2025-05-13 22:14:14 +0200

    finished task no1 in lab3

--- java/lab3/Budynek/src/budynek/Main.java
+++ java/lab3/Budynek/src/budynek/Main.java
@@ -1,11 +1,38 @@
 package budynek;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-    Budynek b = new Budynek("ul. Kwaitowa 10");
-    System.out.println("Budynek: " + b.getAdres());
-    BudynekMieszkalny bm = new BudynekMieszkalny("Polna 123", 4);
-    System.out.println("Budynek: " + bm.getAdres() + ", " + bm.getLiczbaMieszkancow());
-    }
+        // Utworzenie budynku
+        Budynek b = new Budynek("ul. Kwiatowa 10");
+        System.out.println("Budynek: " + b.getAdres());
 
+        // Utworzenie budynku mieszkalnego
+        BudynekMieszkalny bm = new BudynekMieszkalny("ul. Polna 123", 4);
+        System.out.println("Budynek mieszkalny: " + bm.getAdres() + ", " + bm.getLiczbaMieszkancow());
+
+        // Polimorfizm: referencja typu Budynek, obiekt BudynekMieszkalny
+        Budynek b2 = new BudynekMieszkalny("ul. Zielona 2", 8);
+        b2.wypiszInfo();  // Wywoła się wersja z klasy BudynekMieszkalny (override)
+
+        // Lista budynków
+        List<Budynek> listaBudynkow = new ArrayList<>();
+        listaBudynkow.add(b);
+        listaBudynkow.add(0, bm);
+
+        // Liczba elementów
+        System.out.println("\nLiczba budynków w liście: " + listaBudynkow.size());
+
+        // Iteracja i wypisywanie
+        for (Budynek bud : listaBudynkow) {
+            bud.wypiszInfo();
+        }
+
+        // Usuwanie i czyszczenie
+        listaBudynkow.remove(0);
+        listaBudynkow.clear();
+        System.out.println("\nLista po czyszczeniu, liczba budynków: " + listaBudynkow.size());
+    }
 }
